fdd15b2057ac26d34dfc86ae7f72361f326bae29
Tuba Abbasi <tsabbasi@gmail.com>
2017-12-05 15:32:42 -0500

    Optimal bathroom implementation completed

--- count_bathroom_units.m
+++ count_bathroom_units.m
@@ -1,5 +1,6 @@
 function result = count_bathroom_units(num_people, time_spent, bathroom_units)
 patron_waited = 1;
+% loop runs until we have no more patrons waiting
 while patron_waited > 0
         patron_waited = count_patrons_waiting(num_people, time_spent, bathroom_units);
         if patron_waited > 0 && patron_waited < 5 
--- question1.m
+++ question1.m
@@ -1,34 +1,25 @@
 
-numTrials = 5;
-lambda = [.5, 1.0, 1.5];
+numTrials = 10;
+lambda = [.5, 1.0, 5.0];
 
 XMIN = 0;
-XMAX = numTrials;
-YMIN = 0;
+XMAX = 5;
+YMIN = 0.0;
 YMAX= 1.6;
 
 clf;
 figure(1);
-for index=1:numel(lambda)
-    for i=1:1:numTrials
-        probability_mass_function = sample_exp_dist(lambda(index), 1);
-        
-        switch(index)
-            case 1
-                plot(i, probability_mass_function, '+r');
-                hold on;
+x = 0:.1:10;
 
-            case 2
-                plot(i, probability_mass_function, '+b');
-                hold on;
+% exppdf function comes from same toolbox as exprnd
+probability_mass_function1 = exppdf(x, 1/.5);
+plot(x, probability_mass_function1, '+r');
+hold on;
+probability_mass_function2 = exppdf(x, 1/1.0);
+plot(x, probability_mass_function2, '+b');
+hold on;
+probability_mass_function3 = exppdf(x, 1/1.5);
+plot(x, probability_mass_function3, '+g');
+hold on;
 
-            case 3
-                plot(i, probability_mass_function, '+g');
-                hold on;
-
-        end
-        grid on;
-
-    end
-end
 axis([XMIN XMAX YMIN YMAX]);
